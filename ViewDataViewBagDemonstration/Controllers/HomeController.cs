12258f4eb155041ff41a329f54c12111e6916595
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 23:06:51 +0530

    Add project files.

---
+++ ViewDataViewBagDemonstration/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewDataViewBagDemonstration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.userName = "Pragim";
+            ViewData["locationList"] = new List<string>
+            {
+                "Bangalore","Chennai","Hyderabad"
+            };
+
+            return View();
+        }
+
+        public ActionResult EditProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewDataViewBagDemonstration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ViewDataViewBagDemonstration.MvcApplication" Language="C#" %>
